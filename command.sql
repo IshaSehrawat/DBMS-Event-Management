4660c9a019f8fa1ed0dd0f15190b6610880220ea
Raghav Nakra <71371427+ragh-a-void@users.noreply.github.com>
2021-02-22 00:13:18 +0530

    Update command.sql

--- command.sql
+++ command.sql
@@ -6,18 +6,18 @@ show tables;
 
 create table event(
 	event_number int(10),
-    event_name varchar(15),
-    event_date date,
-    first_prize int(10),
-    second_prize int(10),
-    third_prize int(10),
-    start_time time,
-    end_time time,
-    venue varchar(15),
-    budget int(10),
-    requirements varchar(200) default 'Not Decided',
-    event_type varchar(15),
-    primary key(event_number)
+	event_name varchar(15),
+	event_date date,
+	first_prize int(10),
+	second_prize int(10),
+	third_prize int(10),
+	start_time time,
+	end_time time,
+	venue varchar(15),
+	budget int(10),
+	requirements varchar(200) default 'Not Decided',
+	event_type varchar(15),
+	primary key(event_number)
 );
 
 select * from event;
@@ -37,27 +37,70 @@ select * from OC;
 
 create table OT(
 	OT_ID int(10),
-    roll_number int(10),
-    first_name varchar(15),
-    last_name varchar(15),
-    email varchar(30),
-    phone_number varchar(10),
-    event_number int(10),
-    OC_ID int(10) references OC(OC_ID),
-    sponsors_contacted int(10),
-    primary key(OT_ID)
+	roll_number int(10),
+	first_name varchar(15),
+	last_name varchar(15),
+	email varchar(30),
+	phone_number varchar(10),
+	event_number int(10),
+	OC_ID int(10) references OC(OC_ID),
+	sponsors_contacted int(10),
+	primary key(OT_ID)
 );
 
 select * from OT;
 
 create table sponsor(
 	sponsor_number int(10),
-    phone_number varchar(10),
-    email varchar(15),
-    primary key(sponsor_number)
+	    phone_number varchar(10),
+	    email varchar(30),
+	    primary key(sponsor_number)
 );
+
+select * from sponsor;
 
 create table sponsors(
 	event_number int(10) references event(event_number),
-    sponsor_number int(10) references sponsor(sponsor_number)
+    	sponsor_number int(10) references sponsor(sponsor_number)
 );
+
+select * from sponsors;
+
+create table attendees(
+	registration_id	int(10),
+    	first_name varchar(15),
+	last_name varchar(15),
+	email varchar(30),
+	institute_name varchar(30),
+	phone_number varchar(10),
+    	primary key(registration_id)
+);
+
+select * from attendees;
+
+create table volunteer(
+	volunteer_id int(10),
+	roll_number int(10),
+	firstname varchar(15),
+	lastname varchar(15),
+	email varchar(30),
+	institute varchar(30),
+	phone_number varchar(10),
+    	primary key(volunteer_id)
+);
+
+select * from volunteer;
+
+create table participates(
+	registration_id int(10) references attendees(registration_id),
+    	event_number int(10) references event(event_number)
+);
+
+select * from participates;
+
+create table assists(
+	volunteer_id int(10) references volunteer(volunteer_id),
+    	OT_ID int(10) references OT(OT_ID)
+);
+
+select * from assists;
