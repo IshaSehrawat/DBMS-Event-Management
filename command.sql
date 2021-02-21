72110a89fbdd3dcac0b28fab1fd9e7c2e7483859
Raghav Nakra <71371427+ragh-a-void@users.noreply.github.com>
2021-02-22 00:21:39 +0530

    Update command.sql

--- command.sql
+++ command.sql
@@ -6,46 +6,46 @@ show tables;
 
 create table event(
 	event_number int(10),
-	event_name varchar(15),
-	event_date date,
-	first_prize int(10),
-	second_prize int(10),
-	third_prize int(10),
-	start_time time,
-	end_time time,
-	venue varchar(15),
-	budget int(10),
-	requirements varchar(200) default 'Not Decided',
-	event_type varchar(15),
-	primary key(event_number)
+    event_name varchar(15),
+    event_date date,
+    first_prize int(10),
+    second_prize int(10),
+    third_prize int(10),
+    start_time time,
+    end_time time,
+    venue varchar(15),
+    budget int(10),
+    requirements varchar(200) default 'Not Decided',
+    event_type varchar(15),
+    primary key(event_number)
 );
 
 select * from event;
 
 create table OC(
 	OC_ID int(10),
-    first_name varchar(15),
-    last_name varchar(15),
-    email varchar(30),
-    phone_number varchar(10),
-    roll_number int(10),
-    type varchar(15),
-    primary key(OC_ID)
+    	first_name varchar(15),
+    	last_name varchar(15),
+    	email varchar(30),
+    	phone_number varchar(10),
+    	roll_number int(10),
+    	type varchar(15),
+    	primary key(OC_ID)
 );
 
 select * from OC;
 
 create table OT(
 	OT_ID int(10),
-	roll_number int(10),
-	first_name varchar(15),
-	last_name varchar(15),
-	email varchar(30),
-	phone_number varchar(10),
-	event_number int(10),
-	OC_ID int(10) references OC(OC_ID),
-	sponsors_contacted int(10),
-	primary key(OT_ID)
+    roll_number int(10),
+    first_name varchar(15),
+    last_name varchar(15),
+    email varchar(30),
+    phone_number varchar(10),
+    event_number int(10),
+    OC_ID int(10) references OC(OC_ID),
+    sponsors_contacted int(10),
+    primary key(OT_ID)
 );
 
 select * from OT;
@@ -104,3 +104,23 @@ create table assists(
 );
 
 select * from assists;
+
+create table stall_owner(
+	first_name varchar(15),
+    	last_name varchar(15),
+    	email varchar(30),
+	phone_number varchar(10)
+);
+
+select * from stall_owner;
+
+create table stalls(
+	stall_id int(10),
+    	stall_type varchar(30),
+    	location varchar(30),
+    	area varchar(30),
+    	primary key(stall_id)
+);
+
+
+select * from stalls;
