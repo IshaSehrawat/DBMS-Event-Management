2debced94ee7fc9681f400c1f5e8f0ed62fb7fa8
Raghav Nakra <71371427+ragh-a-void@users.noreply.github.com>
2021-02-24 18:01:00 +0530

    Update command.sql

--- command.sql
+++ command.sql
@@ -4,123 +4,135 @@ use fest;
 
 show tables;
 
-create table event(
-	event_number int(10),
-    event_name varchar(15),
-    event_date date,
-    first_prize int(10),
-    second_prize int(10),
-    third_prize int(10),
-    start_time time,
-    end_time time,
-    venue varchar(15),
-    budget int(10),
-    requirements varchar(200) default 'Not Decided',
-    event_type varchar(15),
-    primary key(event_number)
+CREATE TABLE student (
+    roll_number INT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30),
+    email VARCHAR(50),
+    phone_number VARCHAR(10),
+    year INT(1),
+    PRIMARY KEY (roll_number)
 );
 
-select * from event;
-
-create table OC(
-	OC_ID int(10),
-    	first_name varchar(15),
-    	last_name varchar(15),
-    	email varchar(30),
-    	phone_number varchar(10),
-    	roll_number int(10),
-    	type varchar(15),
-    	primary key(OC_ID)
+CREATE TABLE OC (
+    OC_ID INT AUTO_INCREMENT,
+    roll_number INT,
+    type VARCHAR(15),
+    PRIMARY KEY (OC_ID)
 );
 
-select * from OC;
-
-create table OT(
-	OT_ID int(10),
-    roll_number int(10),
-    first_name varchar(15),
-    last_name varchar(15),
-    email varchar(30),
-    phone_number varchar(10),
-    event_number int(10),
-    OC_ID int(10) references OC(OC_ID),
-    sponsors_contacted int(10),
-    primary key(OT_ID)
+CREATE TABLE OT (
+    OT_ID INT AUTO_INCREMENT,
+    roll_number INT,
+    event_number INT,
+    OC_ID INT REFERENCES OC (OC_ID),
+    sponsors_contacted INT,
+    PRIMARY KEY (OT_ID)
 );
 
-select * from OT;
-
-create table sponsor(
-	sponsor_number int(10),
-	    phone_number varchar(10),
-	    email varchar(30),
-	    primary key(sponsor_number)
+CREATE TABLE volunteer (
+    volunteer_id INT,
+    roll_number INT NOT NULL,
+    firstname VARCHAR(15) NOT NULL,
+    lastname VARCHAR(15),
+    email VARCHAR(30) NOT NULL,
+    phone_number VARCHAR(10) NOT NULL,
+    PRIMARY KEY (volunteer_id)
 );
 
-select * from sponsor;
-
-create table sponsors(
-	event_number int(10) references event(event_number),
-    	sponsor_number int(10) references sponsor(sponsor_number)
+CREATE TABLE assists (
+    volunteer_id INT REFERENCES volunteer (volunteer_id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    OT_ID INT REFERENCES OT (OT_ID)
+    ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-select * from sponsors;
-
-create table attendees(
-	registration_id	int(10),
-    	first_name varchar(15),
-	last_name varchar(15),
-	email varchar(30),
-	institute_name varchar(30),
-	phone_number varchar(10),
-    	primary key(registration_id)
-);
-
-select * from attendees;
-
-create table volunteer(
-	volunteer_id int(10),
-	roll_number int(10),
-	firstname varchar(15),
-	lastname varchar(15),
-	email varchar(30),
-	institute varchar(30),
-	phone_number varchar(10),
-    	primary key(volunteer_id)
-);
-
-select * from volunteer;
-
-create table participates(
-	registration_id int(10) references attendees(registration_id),
-    	event_number int(10) references event(event_number)
-);
-
-select * from participates;
-
-create table assists(
-	volunteer_id int(10) references volunteer(volunteer_id),
-    	OT_ID int(10) references OT(OT_ID)
-);
-
-select * from assists;
-
-create table stall_owner(
-	first_name varchar(15),
-    	last_name varchar(15),
-    	email varchar(30),
-	phone_number varchar(10)
-);
-
-select * from stall_owner;
-
-create table stalls(
-	stall_id int(10),
-    	stall_type varchar(30),
-    	location varchar(30),
-    	area varchar(30),
-    	primary key(stall_id)
+CREATE TABLE event (
+    event_number INT AUTO_INCREMENT,
+    event_name VARCHAR(15) NOT NULL,
+    event_date DATE,
+    first_prize INT,
+    second_prize INT,
+    third_prize INT,
+    start_time TIME,
+    end_time TIME,
+    venue VARCHAR(15) DEFAULT 'TBD',
+    budget INT,
+    requirements VARCHAR(200) DEFAULT 'Not Decided',
+    event_type VARCHAR(15) NOT NULL,
+    PRIMARY KEY (event_number)
 );
 
 
-select * from stalls;
+CREATE TABLE participates (
+    registration_id INT REFERENCES attendees (registration_id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    event_number INT REFERENCES event (event_number)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+CREATE TABLE sponsor (
+    sponsor_number INT,
+    email VARCHAR(50),
+    PRIMARY KEY (sponsor_number)
+);
+
+CREATE TABLE sponsor_phone (
+    sponsor_number INT REFERENCES sponsor (sponsor_number)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    phone_number VARCHAR(50)
+);
+
+CREATE TABLE sponsors (
+    event_number INT REFERENCES event (event_number)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    sponsor_number INT REFERENCES sponsor (sponsor_number)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    cash INT
+);
+
+CREATE TABLE in_kind (
+    event_number INT REFERENCES event (event_number)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    sponsor_number INT REFERENCES sponsor (sponsor_number)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    in_kind_details VARCHAR(100)
+);
+
+CREATE TABLE attendees (
+    registration_id INT AUTO_INCREMENT,
+    first_name VARCHAR(15) NOT NULL,
+    last_name VARCHAR(15),
+    email VARCHAR(30) NOT NULL,
+    institute_name VARCHAR(30) DEFAULT 'NA',
+    phone_number VARCHAR(10) NOT NULL,
+    PRIMARY KEY (registration_id)
+);
+alter table attendees auto_increment = 2021000;
+
+CREATE TABLE stall_owner (
+    first_name VARCHAR(15) NOT NULL,
+    last_name VARCHAR(15),
+    email VARCHAR(30) PRIMARY KEY,
+    phone_number VARCHAR(10) NOT NULL,
+    stall_id INT REFERENCES stalls (stall_id)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE stalls (
+    stall_id INT,
+    stall_type VARCHAR(30) NOT NULL,
+    location VARCHAR(30) NOT NULL,
+    stall_area VARCHAR(30) NOT NULL,
+    PRIMARY KEY (stall_id)
+);
+
+show tables;
+
+
+
+
+
+
+
